e4c2176dc82f4f3b00eda09ea6db2bd30ead2475
SanjibSarkarU <95769811+SanjibSarkarU@users.noreply.github.com>
2022-02-20 19:19:03 -0600

    logger

--- testfiles-junk/junk_dataretention-1.py
+++ testfiles-junk/junk_dataretention-1.py
@@ -1,9 +1,22 @@
 import time
-
+import logging
 import junkdataretention
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
-for i in range(1, 100):
-    a = junkdataretention.data_retention(i,i,1, i, retentionsize= 5)
-    print(a)
+file_handler = logging.FileHandler('sample.log')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+for i in range(1, 10):
+    a = junkdataretention.data_retention(i, i, 1, i, retentionsize= 5)
+    # print(a)
+    logger.info('SAVED')
     time.sleep(1)
--- testfiles-junk/junkdataretention.py
+++ testfiles-junk/junkdataretention.py
@@ -1,5 +1,19 @@
 import numpy as np
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('sample.log')
+# file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 def data_retention(*data, retentionsize=0):
     """ Store retentionsize data; cannot save if changes in the data size
@@ -17,15 +31,18 @@ def data_retention(*data, retentionsize=0):
             data_retention.nparray = np.roll(data_retention.nparray, 1, axis=0)
             data_retention.nparray[0] = data
             data_retention.retention_size = retentionsize
+            logger.info(data)
         elif retentionsize == data_retention.retention_size:
             data_retention.nparray = np.roll(data_retention.nparray, 1, axis=0)
             data_retention.nparray[0] = data
+            logger.info(data)
         elif retentionsize < data_retention.retention_size:
             'retentionsize '
             data_retention.nparray = np.resize(data_retention.nparray, (retentionsize, len(data)))
             data_retention.nparray = np.roll(data_retention.nparray, 1, axis=0)
             data_retention.nparray[0] = data
             data_retention.retention_size = retentionsize
+            logger.info(data)
         return data_retention.nparray[~np.isnan(data_retention.nparray).any(axis=1)]
     else:
         raise RuntimeError('The number of input arguments in data_retention changed.' +
@@ -33,6 +50,9 @@ def data_retention(*data, retentionsize=0):
 
 
 if __name__ == '__main__':
+
+
+
     for i in range(1, 20):
         if i < 5:
             a = data_retention(i, i, i, retentionsize=2)
